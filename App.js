5d20f2a2a62a90c7ae6d76b47fc326c69c7896b3
Dovile Mart <doviliu@gmail.com>
2023-01-30 13:27:07 +0200

    Historia-toiminto lisätty

--- App.js
+++ App.js
@@ -1,56 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import { useState, useRef } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
+  
+  const [eka, setEka] = useState('');
+  console.log(eka);
+  const [toka, setToka] = useState('');
+  console.log(toka);
+  
+  const initialFocus = useRef(null);
 
-  let [tulos, setTulos] = useState(0);
+  let [tulos, setTulos] = useState('');
+  const [data, setData] = useState([]);
 
   const sumNumbers = () => {
     tulos = Number(eka) + Number(toka);
-    console.log('Summa ' + tulos);
-    setTulos(tulos);  //EI tulos=setTulos(tulos)
-    console.log('Summatulos setTulos jälkeen ' + tulos);
+    console.log('Summa: ' + tulos);
+    setTulos(tulos);
+    const summa = (eka + ' + ' + toka + ' = ' + tulos);
+    console.log(summa);
+    setData([...data, {key: summa}]);
+    setEka('');
+    setToka('');
+    initialFocus.current.focus();
   } 
 
   const minusNumbers = () => {
     tulos = Number(eka) - Number(toka);
-    console.log('Vähennys ' + tulos);
+    console.log('Vähennys: ' + tulos);
+    const vahennys = (eka + ' - ' + toka + ' = ' + tulos);
+    console.log(vahennys);
     setTulos(tulos);
-    console.log('Vähennystulos setTulos jälkeen ' + tulos);
+    setData([...data, {key: vahennys}]);
+    setEka('');
+    setToka('');
+    initialFocus.current.focus();
   }  
     
-  const [eka, setEka] = useState(0); //('')-merkkijono
-  console.log(eka);
-  const [toka, setToka] = useState(0); //näyttää nollan jos on tyhjä
-  console.log(toka);
+  
 
   return (
     <View style={styles.container}>
-      <View style={styles.inrow}>
-        <Text>Result: </Text>
-        <Text>{tulos}</Text>
       
+      <View style={styles.container}>
+        <View style={styles.inrow}>
+          <Text>Result: </Text>
+          <Text>{tulos}</Text>
+        </View>
+        <View>
+          <TextInput
+            style={styles.input}
+            ref={initialFocus}  
+            value={eka}
+            onChangeText={eka => setEka(eka)}    
+            keyboardType="numeric"
+          />
+          <TextInput
+            style={styles.input}
+            value={toka}
+            onChangeText={toka => setToka(toka)} 
+            keyboardType="numeric"
+          />
+        </View>
+      
+        <View style={styles.inrow}>
+          <View style={styles.button}>
+            <Button title="+" onPress={sumNumbers} /> 
+          </View>
+          <View style={styles.button}>
+            <Button title="-" onPress={minusNumbers} />
+          </View>
+        </View>
       </View>
-      <View>
-        <TextInput
-          style={styles.input}  
-          value={eka}
-          onChangeText={eka => setEka(eka)}    
-          keyboardType="numeric"
+
+      <View style={styles.container2}>
+        <Text>History</Text>
+
+        <FlatList style={styles.list}
+          data={data}
+          renderItem={({item}) => <Text>{item.key}</Text>}
+          keyExtractor={(item, index) => index.toString()}
         />
-        <TextInput
-          style={styles.input}
-          value={toka}
-          onChangeText={toka => setToka(toka)} 
-          keyboardType="numeric"
-        />
       </View>
-      <View style={styles.inrow}>
-        <Button title="+" onPress={sumNumbers} /> 
-        <Text>   </Text>
-        <Button title="-" onPress={minusNumbers} />
-      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -63,14 +96,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    flex: 2
+  },
   input: {
     width:150, 
     border: "grey", 
-    borderWidth:1
+    borderWidth:1,
+    margin: 1,
+    paddingStart: 15
   },
   inrow: {
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around'
+    margin: 3
+    //alignItems: 'center',
+    //justifyContent: 'space-around'
+  },
+  button: {
+    margin:5
+  },
+  list: {
+    alignSelf: 'center',
+    textAlign: 'left',
+    padding: 2
   }
 });
